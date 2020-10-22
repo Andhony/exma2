8a52ecfe3cb7ad3e27de1b84b943f1d9cff3695c
Ernesto Ayala <ernmardav04@gmail.com>
2020-10-21 23:12:08 -0600

    Creacion de activity score y juego

---
+++ app/src/main/java/com/uso/exma2/InicioJuego.java
@@ -0,0 +1,14 @@
+package com.uso.exma2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioJuego extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_juego);
+    }
+}
