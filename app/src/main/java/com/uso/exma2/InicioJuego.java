c701d348e63e0e3d779ea092b7153e376149f1aa
Jeymi <jeymibrisel@gmail.com>
2020-10-22 14:32:44 -0600

    Pequeña modificacion

--- app/src/main/java/com/uso/exma2/InicioJuego.java
+++ app/src/main/java/com/uso/exma2/InicioJuego.java
@@ -45,27 +45,28 @@ public class InicioJuego extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                    int numuser;
+            int numuser;
 
-                    numuser = Integer.parseInt(numero.getText().toString());
-                    if (numuser < 0 || numuser > 5) {
-                        Toast.makeText(InicioJuego.this, "Este no es un numero valido", Toast.LENGTH_SHORT).show();
-                    } else if (aleatorio < numuser) {
-                        Toast.makeText(InicioJuego.this, "Ingrese un numero mas bajo", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(InicioJuego.this, "Ingrese un numero mas alto", Toast.LENGTH_SHORT).show();
-                    }
-                        if (aleatorio == numuser) {
-                        jugar.setEnabled(false);
-                        Toast.makeText(InicioJuego.this, "Has ganado!", Toast.LENGTH_SHORT).show();
-                        Toast.makeText(InicioJuego.this, "El numero aleatorio es:" + aleatorio, Toast.LENGTH_SHORT).show();
-                    }
-                    intentos = intentos - 1;
-                    Toast.makeText(InicioJuego.this, "Intentos restantes: " + intentos, Toast.LENGTH_SHORT).show();
-                    if (intentos == 0 && aleatorio != numuser){
-                        jugar.setEnabled(false);
-                        Toast.makeText(InicioJuego.this, "Has perdido: ", Toast.LENGTH_SHORT).show();
-                        Toast.makeText(InicioJuego.this, "El numero aleatorio es:" + aleatorio, Toast.LENGTH_SHORT).show();
+            numuser = Integer.parseInt(numero.getText().toString());
+
+            if (numuser < 0 || numuser > 5 && lblNivel.getText().toString() == "Fácil") {
+                Toast.makeText(InicioJuego.this, "Este no es un numero valido", Toast.LENGTH_SHORT).show();
+            } else if (aleatorio < numuser) {
+                Toast.makeText(InicioJuego.this, "Ingrese un numero mas bajo", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(InicioJuego.this, "Ingrese un numero mas alto", Toast.LENGTH_SHORT).show();
+            }
+            if (aleatorio == numuser) {
+                jugar.setEnabled(false);
+                Toast.makeText(InicioJuego.this, "Has ganado!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(InicioJuego.this, "El numero aleatorio es:" + aleatorio, Toast.LENGTH_SHORT).show();
+            }
+                intentos = intentos - 1;
+                Toast.makeText(InicioJuego.this, "Intentos restantes: " + intentos, Toast.LENGTH_SHORT).show();
+                if (intentos == 0 && aleatorio != numuser){
+                    jugar.setEnabled(false);
+                    Toast.makeText(InicioJuego.this, "Has perdido: ", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(InicioJuego.this, "El numero aleatorio es:" + aleatorio, Toast.LENGTH_SHORT).show();
 
                 }
             }
@@ -88,15 +89,14 @@ public class InicioJuego extends AppCompatActivity {
                 var= (int)(Math.random()*5+1);
                 break;
             case "Medio":
-                var= (int)(Math.random()*10+6);
+                var= (int)(Math.random()*10+1);
                 break;
             case "Difícil":
                 var= (int)(Math.random()*15+1);
                 break;
             default :
-                var =0;
-                break;
 
+                break;
         }
         return var;
     }
