e1f3a16edae43e7d2a29f33a290785317305b23b
Ernesto Ayala <ernmardav04@gmail.com>
2020-10-22 10:44:21 -0600

    Interfaz de juego

--- app/src/main/java/com/uso/exma2/MainActivity.java
+++ app/src/main/java/com/uso/exma2/MainActivity.java
@@ -5,13 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnJuego;
+    Button btnConfig;
+    Button btnPuntaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.btnJuego = findViewById(R.id.btnjuego);
+        this.btnConfig = findViewById(R.id.btnconfiguracion);
+        this.btnPuntaje = findViewById(R.id.btnpuntaje);
+
+        this.btnJuego.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent mIntent = new Intent(MainActivity.this,InicioJuego.class);
+                startActivity(mIntent);
+            }
+
+        });
+
+        this.btnPuntaje.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent mIntent = new Intent(MainActivity.this,ScoreActivity.class);
+                startActivity(mIntent);
+            }
+
+        });
+
+        this.btnConfig.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent mIntent = new Intent(MainActivity.this,Configuraciones.class);
+                startActivity(mIntent);
+            }
+
+        });
     }
 
     public void configuraciones_onClick(View v){
