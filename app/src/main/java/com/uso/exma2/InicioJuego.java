f92550024a8243b41d0dfd620913b8f5c0e77d09
Jeymi <jeymibrisel@gmail.com>
2020-10-22 03:21:05 -0600

    Configuraciones funcional, Inicio juego ya recupera los datos del shared Preference

--- app/src/main/java/com/uso/exma2/InicioJuego.java
+++ app/src/main/java/com/uso/exma2/InicioJuego.java
@@ -2,13 +2,43 @@ package com.uso.exma2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class InicioJuego extends AppCompatActivity {
+    public static final String ARCHIVO = "Configuracion";
+    public static final String KEY_NICKNAME = "NickName";
+    public static final String KEY_DIFICULTAD = "Dificultad";
+
+    public SharedPreferences configuraciones;
+
+    private TextView lblNickname;
+    private TextView lblNivel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_juego);
+
+        this.lblNickname = findViewById(R.id.lblNickname);
+        this.lblNivel = findViewById(R.id.lblNivel);
+
+        this.configuraciones = getSharedPreferences(ARCHIVO,MODE_PRIVATE);
+
+        //Validamos que exista una instancia de la configuración
+        if(configuraciones != null){
+            this.lblNickname.setText(configuraciones.getString(KEY_NICKNAME, ""));
+            this.lblNivel.setText(configuraciones.getString(KEY_DIFICULTAD, ""));
+        }else {
+            Toast.makeText(this, "Es nulo", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void Valida(){
+
     }
 }
--- app/src/main/java/com/uso/exma2/MainActivity.java
+++ app/src/main/java/com/uso/exma2/MainActivity.java
@@ -2,7 +2,9 @@ package com.uso.exma2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void configuraciones_onClick(View v){
+        Intent frmConfiguracion = new Intent(this, Configuraciones.class);
+        startActivity(frmConfiguracion);
+    }
+
+    public void iniciarJuego_onClick(View v){
+        Intent frmIniciar = new Intent(this, InicioJuego.class);
+        startActivity(frmIniciar);
     }
 }
